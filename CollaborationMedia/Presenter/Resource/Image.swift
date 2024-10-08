f88fb9be061ffe974cff5d63b5ef647fe7c3fbea
Alpaca38 <137505484+Alpaca38@users.noreply.github.com>
2024-10-08 21:19:24 +0900

    [Add] CIColor 및 Icon Image 리소스 세팅

---
+++ CollaborationMedia/Presenter/Resource/Image.swift
@@ -0,0 +1,19 @@
+//
+//  Image.swift
+//  CollaborationMedia
+//
+//  Created by 조규연 on 10/8/24.
+//
+
+import UIKit
+
+enum Image {
+    static let play = UIImage(systemName: "play")
+    static let magnifyingglass = UIImage(systemName: "magnifyingglass")
+    static let sparklesTV = UIImage(systemName: "sparkles.tv")
+    static let house = UIImage(systemName: "house")
+    static let playCircle = UIImage(systemName: "play.circle")
+    static let squareAndArrowDown = UIImage(systemName: "square.and.arrow.down")
+    static let faceSmiling = UIImage(systemName: "face.smiling")
+    static let plus = UIImage(systemName: "plus")
+}
